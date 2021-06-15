903d31367894cb37bc876a9c89995af634a4a1df
angelo-sant0s <Tifaislove123!>
2021-06-15 16:27:33 +0100

    UI

---
+++ chat.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <!-- java -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+
+    <!-- meta tags -->
+    <meta charset="UTF-8">
+    <meta content="Angelo Santos, André Domingues, Juliana Assis, Luis Simoes" name="author">
+    <meta content="Explain, Universidade de Aveiro, UA, Explicações" name="keywords">
+    <meta content="Website dedicado à entreajuda de estudantes do curso de NTC da Universidade de Aveiro" name="description">
+
+    <meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport">
+
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="fontawesome-free/css/all.css">
+
+    <!-- estilos próprios -->
+    <link href="css/os_meus_estilos.css" rel="stylesheet">
+
+    <!--Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Exo+2&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=PT+Sans&display=swap" rel="stylesheet">
+
+    <title>Chat</title>
+</head>
+<body>
+
+<nav id="navchat" class="navbar d-flex navbar-expand-md bgClaro">
+    <a class="navbar-brand" href="#">
+        <img src="imgs/logo.svg" alt="logo" class="img-fluid px-2" width="90px">
+    </a>
+    <div class="justify-content-center d-flex d-md-none justify-content-md-right">
+        <button type="button" class="btn"><i class="fas fa-bell fa-2x roxinho mx-2"></i></button>
+        <button type="button" class="btn"><a href="chat.html"><i class="fas fa-comment fa-2x roxinho mx-2"></i></a></button>
+    </div>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="fas fa-bars fa-2x roxinho"></i>
+    </button>
+    <div class="collapse navbar-collapse d-md-flex justify-content-md-center" id="navbarSupportedContent">
+        <ul class="navbar-nav text-right">
+            <li class="nav-item">
+                <a class="nav-link" href="home.html">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="perfil.html">Perfil</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="cadeiras.html">Cadeiras</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="tickets.html">Ticket</a>
+            </li>
+            <li class="nav-item d-md-none pt-2">
+                <a href="index.html"><i class="fas fa-sign-out-alt fa-2x roxinho"></i></a>
+            </li>
+        </ul>
+    </div>
+    <div class="justify-content-center d-none d-md-flex justify-content-md-right">
+        <button type="button" class="btn"><i class="fas fa-bell fa-2x roxinho mx-2"></i></button>
+        <button type="button" class="btn"><a href="chat.html"><i class="fas fa-comment fa-2x roxinho mx-2"></i></a></button>
+    </div>
+</nav>
+
+
+
+</div>
+<main class="container-fluid texto cor5" id="mainchat">
+    <section class="row">
+        <!-- conversas -->
+        <article id="conversaSection" class="col-12 col-sm-12 col-md-12 col-lg-3 col-xl-3 bg-person1">
+            <section class="row  borders4" id="conversatitle">
+                <article class="col-12 text-center p-3 bg-inputzone" >Conversa</article>
+            </section>
+
+            <section id="Conversas" class="row overflow-auto borders4">
+                <article class="col-12">
+                    <section id="joao" class="row p-2 custom-borderb cursorPointer">
+                        <article class="col-2 p-0 please ">
+                            <img src="imgs/face1.jpg" class="img-fluid faceIcon">
+                        </article>
+
+                        <article class="col-10 p-0 pl-1 justify-content-between">
+                            <section class="row m-0 h-100">
+                                <article class="col-8 p-0">
+                                    <div class="">João</div>
+                                </article>
+
+                                <article class="col-4 p-0">
+                                    <div class=" text-right">14:20</div>
+                                </article>
+
+                                <article class="col-12 p-0">
+                                    <div class="">LAB4 - Funções globais</div>
+                                </article>
+                            </section>
+                        </article>
+                    </section>
+
+
+
+
+                </article>
+
+
+            </section>
+        </article>
+
+        <!-- chat -->
+        <article id="chatSection" class="col-12 col-sm-12 col-md-12 col-lg-9 col-xl-9 bg-light  ">
+            <section class="row  borders4 bg-inputzone" id="chattitle">
+                <article class="col-1 align-items-center d-flex text-right" id="backtbn"><i class="d-inline-block d-lg-none fas fa-chevron-left arrowsize"></i></article>
+                <article class="col-10 text-center p-3" > Chat</article>
+            </section>
+
+            <section id="chatRow" class="row chatRow justify-content-center p-3 bg-chat-format">
+
+                <!-- resumo topioc -->
+                <article class="col-7">
+                    <div class="resumoTop textoClaro bg-roxo borderElement font-italic text-center p-3 mx-5">
+                        As minhas dúvidas são as seguintes: em primeiro lugar, qual é a diferença entre scope de bloco e scope de função? E já agora, o conceito de scope de bloco sempre exisitu ou surgiu no EcmaScript6 com o aparecimento do let e do const?
+                    </div>
+                </article>
+
+                <!-- divisao de 24h -->
+                <article class="col-7 text-center font-italic text-secondary">
+                    <hr>
+                    Ontem às 14:10
+                </article>
+
+                <!-- corpo da mensagem -->
+                <article class="col-12">
+
+
+                    <section class="row mt-2">
+                        <article class="col-1 p-1 text-center">
+                            <img src="imgs/face1.jpg" class="img-fluid faceIcon">
+                        </article>
+                        <article class="col-8 p-1">
+                            <div class="p-2 bg-person1 borderElement">Olá Mafalda como estás? Quando estás disponivel para marcarmos uma videochamada?</div>
+                        </article>
+                    </section>
+
+
+                    <section class="row mt-2 justify-content-end">
+                        <article class="col-8 p-1">
+                            <div class="p-2 bg-person2 borderElement">Olá João. Obrigada pela ajuda! Posso agora se tu puderes!</div>
+                        </article>
+                        <article class="col-1 p-1 text-center">
+                            <img src="imgs/face2.jpg" class="img-fluid faceIcon">
+                        </article>
+                    </section>
+
+                    <section class="row mt-2">
+                        <article class="col-1 p-1 text-center">
+                            <img src="imgs/face1.jpg" class="img-fluid faceIcon">
+                        </article>
+                        <article class="col-8 p-1">
+                            <div class="p-2 bg-person1 borderElement">Pode ser! vamos então.</div>
+                        </article>
+                    </section>
+
+                </article>
+
+
+                <!-- Indicação de chamada -->
+                <article class="col-12 my-3 text-center">
+                    <hr>
+                    <a href="videochamada.html" class="textoClaro"><span class="py-2 px-3 mt-2 bg-azul2 borderElement d-inline-block">Chamada iniciada</span></a>
+                </article>
+
+            </section>
+
+            <section id="inputZone" class="row py-2 px-3 bg-inputzone">
+                <article class="col-9">
+                    <input id="chatInput" class="borderElement p-1 pl-3 borderPic Valign">
+                </article>
+                <article class="col-1">
+                    <i class="fas fa-arrow-alt-circle-right iconsSize Valign"></i>
+                </article>
+                <article class="col-1">
+                    <i class="fas fa-images iconsSize Valign"></i>
+                </article>
+                <article class="col-1">
+                    <i class="fas fa-video iconsSize Valign"></i>
+                </article>
+
+
+            </section>
+
+        </article>
+
+
+    </section>
+
+</main>
+
+
+<!-- jQuery first, then Popper.js,then Bootstrap JS -->
+<script src="js/jquery-3.4.1.slim.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+<!-- js nossos -->
+<script src="js/js.js"></script>
+</body>
+</html>
+
---
+++ pedido_tickets.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <!-- java -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+
+    <!-- meta tags -->
+    <meta charset="UTF-8">
+    <meta content="Angelo Santos, André Domingues, Juliana Assis, Luis Simoes" name="author">
+    <meta content="Explain, Universidade de Aveiro, UA, Explicações" name="keywords">
+    <meta content="Website dedicado à entreajuda de estudantes do curso de NTC da Universidade de Aveiro" name="description">
+
+    <meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport">
+
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="fontawesome-free/css/all.css">
+
+    <!-- estilos próprios -->
+    <link href="css/os_meus_estilos.css" rel="stylesheet">
+
+    <!--Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Exo+2&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=PT+Sans&display=swap" rel="stylesheet">
+
+    <title>Submissão de ticket</title>
+</head>
+<body class="cinzaClaroBg">
+<nav class="navbar sticky-top d-flex navbar-expand-md bgClaro">
+    <a class="navbar-brand" href="home.html">
+        <img src="imgs/logo.svg" alt="logo" class="img-fluid px-2" width="90px">
+    </a>
+    <div class="justify-content-center d-flex d-md-none justify-content-md-right">
+        <button type="button" class="btn"><i class="fas fa-bell fa-2x roxinho mx-2"></i></button>
+        <button type="button" class="btn"><a href="chat.html"><i class="fas fa-comment fa-2x roxinho mx-2"></i></a></button>
+    </div>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="fas fa-bars fa-2x roxinho"></i>
+    </button>
+    <div class="collapse navbar-collapse d-md-flex justify-content-md-center" id="navbarSupportedContent">
+        <ul class="navbar-nav text-right">
+            <li class="nav-item">
+                <a class="nav-link" href="home.html">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="perfil.html">Perfil</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="cadeiras.html">Cadeiras</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link corazul" href="tickets.html">Ticket</a>
+            </li>
+            <li class="nav-item d-md-none pt-2">
+                <a href="index.html"><i class="fas fa-sign-out-alt fa-2x roxinho"></i></a>
+            </li>
+        </ul>
+    </div>
+    <div class="justify-content-center d-none d-md-flex justify-content-md-right">
+        <button type="button" class="btn"><i class="fas fa-bell fa-2x roxinho mx-2"></i></button>
+        <button type="button" class="btn"><a href="chat.html"><i class="fas fa-comment fa-2x roxinho mx-2"></i></a></button>
+    </div>
+</nav>
+
+<main class="container textoEscuro texto fundoClaro borderElement my-5 p-4 p-xl-5">
+    <section class="row justify-content-center">
+        <article class="col-12 mt-3">
+            <h3 class="titulo font-weight-bold text-center mb-4">Submissão de tickets</h3>
+            <p class="small font-italic  ml-2 mb-0 text-center">Efetua aqui um pedido de esclarecimento de dúvidas que tenhas acerca de qualquer cadeira.<br>Posteriormente aguarda pela resposta de um dos mentores.</p>
+        </article>
+
+        <!-- Falta trocar as arrows nos collapses quando abre e fecha -->
+        <article class="col-12 col-md-10 col-lg-8 mt-5">
+            <select class="custom-select roxinhob textoClaro borderElement px-4 dropTicket" id="urgenciaEscolha">
+                <option selected>Urgência...</option>
+                <option value="1">Baixa</option>
+                <option value="2">Média</option>
+                <option value="3">Alta</option>
+            </select>
+
+            <select class="custom-select rosinha textoClaro mt-2 borderElement px-4 dropTicket" id="cadeiraEscolha">
+                <option selected>Cadeira...</option>
+                <option value="1">Ergonomia Cognitiva</option>
+                <option value="2">Laboratório Multimédia 3</option>
+                <option value="3">Sociologia da Comunicação</option>
+                <option value="4">Sistemas de Comunicação Multimédia I</option>
+            </select>
+        </article>
+
+        <article class="col-12 col-md-10 col-lg-8 mt-4">
+            <div class="cinzaClaroBg borderElement py-4 px-3 p-sm-4 p-xl-5">
+                <form>
+                    <div class="form-group">
+                        <label for="FormControlInput1" class="textoAzul1 font-weight-bold">Título</label>
+                        <input type="text" class="form-control borderElement font-italic px-3 dropTicket" id="FormControlInput1" placeholder="Refere o assunto...">
+                    </div>
+                    <div class="form-group">
+                        <label for="FormControlTextarea1" class="textoAzul1 font-weight-bold mt-2">Mensagem</label>
+                        <textarea class="form-control borderElement font-italic p-3" id="FormControlTextarea1" maxlength="800" rows="8" placeholder="Descreve a dúvida detalhadamente..."></textarea>
+                        <div class="d-flex justify-content-between small mt-2">
+                            <a href="#" class="text-secondary cursor"><i class="fas fa-paperclip fa-sm mr-1"></i> anexar ficheiro</a>
+                            <div id="count">
+                                <span id="current_count">0</span>
+                                <span id="maximum_count">/ 800</span>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+
+                <div class="text-center mb-3 mt-5">
+                    <button class="btn bg-danger submitCirculo mr-1" id="apagarTicket"><i class="fas fa-times fa-lg textoClaro"></i></button>
+                    <button class="btn bg-success submitCirculo ml-1" id="enviarTicket"><i class="fas fa-chevron-right fa-lg textoClaro"></i></button>
+                    <p class="small font-italic mt-2" id="acao">Ação.</p>
+                </div>
+            </div>
+        </article>
+    </section>
+
+</main>
+
+<!-- Footer -->
+<footer class="container-fluid bgEscuro p-3">
+    <section class="row justify-content-md-between">
+        <article class="col-md-2 pt-4 pl-2 pl-md-5 text-white text-center text-md-left">
+            <h5 class="footer-brand pl-2 pb-3 "><img src="imgs/logobranco.svg" width="80px" alt="img2">
+            </h5>
+        </article>
+        <article class="col-md-7 pt-0 pt-md-4 pl-2 pl-md-0 text-white text-center text-md-left">
+            <a href="#"><i class="fab fa-facebook-f p-3"></i></a>
+            <a href="#"><i class="fab fa-twitter p-3"></i></a>
+            <a href="#"><i class="fab fa-instagram p-3"></i></a>
+        </article>
+        <article class="col-md-3 text-white">
+            <section class="row">
+                <article class="col-12 col-md-6 p-1 font-weight-normal text-center pt-5">
+                    <a href="#" class="tamanho">
+                        <h4>Sobre</h4>
+                    </a>
+                </article>
+                <article class="col-12 col-md-6 p-1 font-weight-normal text-center py-5">
+                    <a href="#" class="tamanho">
+                        <h4>Contactos</h4>
+                    </a>
+                </article>
+            </section>
+        </article>
+    </section>
+    <section class="row">
+        <article class="col-12 text-white">
+            <p class="text-right h4 font-weight-normal pt-4 tamanho ">&commat; 2021 explain.ua, All Rights Reserved</p>
+        </article>
+    </section>
+</footer>
+
+<!-- jQuery first, then Popper.js,then Bootstrap JS -->
+<script src="js/jquery-3.4.1.slim.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+<script type="text/javascript">
+    $('textarea').keyup(function() {
+        var characterCount = $(this).val().length,
+            current_count = $('#current_count'),
+            maximum_count = $('#maximum_count'),
+            count = $('#count');
+        current_count.text(characterCount);
+    });
+
+
+    document.getElementById('apagarTicket').onmouseover = function () {
+        document.getElementById('acao').style.visibility = "visible";
+        document.getElementById('acao').innerHTML = "Apaga todos os campos preenchidos.";
+    };
+    document.getElementById('enviarTicket').onmouseover = function () {
+        document.getElementById('acao').style.visibility = "visible";
+        document.getElementById('acao').innerHTML = "Envia o ticket de esclarecimento.";
+    };
+
+    document.getElementById('apagarTicket').onmouseout = function () {
+        document.getElementById('acao').style.visibility = "hidden";
+    };
+    document.getElementById('enviarTicket').onmouseout = function () {
+        document.getElementById('acao').style.visibility = "hidden";
+    };
+</script>
+
+</body>
+</html>
