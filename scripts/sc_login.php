95bf56bc2905778f5045c3dd476eac397a3224c6
angelo-sant0s <Tifaislove123!>
2021-06-16 11:58:27 +0100

    scripts login/logout e register feitos

--- scripts/sc_login.php
+++ scripts/sc_login.php
@@ -1,1 +1,52 @@
 <?php
+require_once "../connections/connections.php";
+
+if (isset($_POST["username"]) && isset($_POST["password"])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $link = new_db_connection();
+
+    $stmt = mysqli_stmt_init($link);
+
+    $query = "SELECT password_hash, ref_id_roles FROM users WHERE username LIKE ?";
+
+    if (mysqli_stmt_prepare($stmt, $query)) {
+        mysqli_stmt_bind_param($stmt, 's', $username);
+
+        if (mysqli_stmt_execute($stmt)) {
+
+            mysqli_stmt_bind_result($stmt, $password_hash, $perfil);
+
+            if (mysqli_stmt_fetch($stmt)) {
+                if (password_verify($password, $password_hash)) {
+                    // Guardar sessão de utilizador
+                    session_start();
+                    $_SESSION["username"] = $username;
+                    $_SESSION["role"] = $perfil;
+
+                    // Feedback de sucesso
+                    header("Location: ../index.php");
+                } else {
+                    // Password está errada
+                    echo "Incorrect credentials!";
+                    echo "<a href='../index.php'>Try again</a>";
+                }
+            } else {
+                // Username não existe
+                echo "Incorrect credentials!";
+                echo "<a href='../index.php'>Try again</a>";
+            }
+        } else {
+            // Acção de erro
+            echo "Error:" . mysqli_stmt_error($stmt);
+        }
+    } else {
+        // Acção de erro
+        echo "Error:" . mysqli_error($link);
+    }
+    mysqli_stmt_close($stmt);
+    mysqli_close($link);
+} else {
+    echo "Campos do formulário por preencher";
+}
