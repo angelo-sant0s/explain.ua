608767fd3b09bbc0bd38e82439c43fe3af1ac195
angelo-sant0s <angelosantos@ua.pt>
2021-07-18 23:52:40 +0100

    Home sem seaarch bar

--- topico.php
+++ topico.php
@@ -23,9 +23,11 @@ $link = new_db_connection();
 
 $stmt = mysqli_stmt_init($link);
 
-$query = "SELECT ticket.titulo, ticket.corpo_mensagem,HOUR(TIMEDIFF(NOW(), topico.data_publicacao)), MINUTE(TIMEDIFF(NOW(), topico.data_publicacao)), utilizador.username, utilizador.id_utilizador, topico.pontuacao FROM ticket 
+$query = "SELECT ticket.titulo, ticket.corpo_mensagem,HOUR(TIMEDIFF(NOW(), topico.data_publicacao)), MINUTE(TIMEDIFF(NOW(), topico.data_publicacao)), utilizador.username, utilizador.id_utilizador, topico.pontuacao, topico.id_topico, cadeira.imagem
+FROM ticket 
 INNER JOIN utilizador ON ticket.utilizador_id_utilizador = utilizador.id_utilizador
 INNER JOIN topico ON topico.id_topico = ticket.topico_id_topico
+INNER JOIN cadeira ON cadeira.id_cadeira = ticket.cadeira_id_cadeira
 WHERE id_ticket = ?";
 
 if (isset($_GET["id"])){
@@ -35,7 +37,7 @@ $id_ticket = $_GET["id"];
 if (mysqli_stmt_prepare($stmt,$query)){
     mysqli_stmt_bind_param($stmt, 'i' , $id_ticket);
     mysqli_stmt_execute($stmt);
-    mysqli_stmt_bind_result( $stmt, $titulo,$corpo, $publishing_hour,$publishing_minute, $autor, $idauthor, $score);
+    mysqli_stmt_bind_result( $stmt, $titulo,$corpo, $publishing_hour,$publishing_minute, $autor, $idauthor,$score, $id_topico, $cadeira_img);
 } else {
     echo "ERROR: ". mysqli_error($link);
 }
@@ -48,7 +50,7 @@ while (mysqli_stmt_fetch($stmt)){
         <article class="col-11 col-md-9 mt-3">
             <section class="row">
                 <div class="col-12 col-sm-3 col-lg-2 text-center">
-                    <img src="imgs/icon_lab4.png" height="100px" width="100px">
+                    <img src="imgs/<?=$cadeira_img?>" height="100px" width="100px">
                 </div>
                 <!-- falta upvote/downvote -->
                 <div class="col-10 col-sm-7 col-lg-8 mt-3 pt-3 pt-sm-0">
@@ -83,6 +85,10 @@ while (mysqli_stmt_fetch($stmt)){
         </article>
 
     </section>
+    <?php
+         }
+    }
+?>
 
     <section class="row justify-content-center">
         <article class="col-12 col-md-11 mt-4">
@@ -114,9 +120,28 @@ while (mysqli_stmt_fetch($stmt)){
             </div>
         </article>
     </section>
-
     <section class="row justify-content-center">
         <article class="col-12 col-md-11 mt-4">
+<?php
+
+$stmt2 = mysqli_stmt_init($link);
+
+$query2 = "SELECT comentario.texto, utilizador.username, comentario.pontuacao, utilizador.foto_perfil, HOUR(TIMEDIFF(NOW(),comentario.data_envio)), MINUTE(TIMEDIFF(NOW(), comentario.data_envio)) 
+FROM comentario
+INNER JOIN utilizador ON comentario.utilizador_id_utilizador = utilizador_id_utilizador
+INNER JOIN topico On topico.id_topico = comentario.topico_id_topico
+WHERE topico.id_topico = ? 
+ORDER BY comentario.pontuacao DESC LIMIT 1";
+
+if (mysqli_stmt_prepare($stmt2,$query2)){
+    mysqli_stmt_bind_param($stmt2, 'i' , $id_topico);
+    mysqli_stmt_execute($stmt2);
+    mysqli_stmt_bind_result( $stmt2, $top_comment,$top_user, $top_score,$top_pfp, $top_envio_hora,$top_envio_minuto);
+} else {
+    echo "ERROR: ". mysqli_error($link);
+}
+while (mysqli_stmt_fetch($stmt2)){
+?>
 
             <div class="cinzaClaroBg borderElement pb-3">
                 <div class="roxinhob borderElement textoClaro titulo py-4 px-3 p-sm-4 p-xl-5">
@@ -126,145 +151,159 @@ while (mysqli_stmt_fetch($stmt)){
                 <div class="fundoClaro borderElement m-3 py-4 px-3 p-sm-4 p-xl-5">
                     <section class="row">
                         <div class="col-12">
-                            <img class="float-left" src="imgs/icon_avatar.png" height="40px" width="40px">
-                            <p class="float-left font-weight-bold m-0 pt-2 pl-2">Luísa Amaral</p>
+                            <img class="float-left" src="imgs/<?=$top_pfp?>" height="40px" width="40px">
+                            <p class="float-left font-weight-bold m-0 pt-2 pl-2"><?=$top_user?></p>
                         </div>
 
                         <div class="col-12 mt-4">
-                            <p>A título de curiosidade, também é possível através de prepared statements com PDO:</p>
-                            <code class="textoRosinha">
-                                $db = new PDO('mysql:host=localhost dbname=teste', 'usuario', 'senha');
-                            </code>
-
+                            <p><?=$top_comment?></p>
                             <div class="mt-4 pr-2 mb-0 d-block">
                                 <div class="float-right">
                                     <i class="fas fa-angle-up fa-sm d-block cursor"></i>
+                                    <?= $top_score ?>
                                     <i class="fas fa-angle-down fa-sm d-block cursor"></i>
                                 </div>
-                                <span class="float-left font-italic small text-secondary pt-2">Postado há 5 horas</span>
+                                <span class="float-left font-italic small text-secondary pt-2">
+                                    <?php
+                                    if ($top_envio_hora == 0){
+                            echo "Postado há ".$top_envio_minuto." minutos"; }
+                        else if ($top_envio_hora > 0 && $top_envio_hora < 24){
+                            echo "Postado há ".$top_envio_hora ." horas";}
+                        else if ($top_envio_hora < 24*7 && $top_envio_hora > 24){
+                            $top_envio_dia = intval(($top_envio_hora / 24));
+                            echo "Postado há ".$top_envio_dia." dias";
+                        }else{
+                            $top_envio_week = intval(($top_envio_hora / (24*7)));
+                            echo  "Postado há ". $top_envio_week." semanas";
+                        }
+                        ?>
+                                </span>
                             </div>
                         </div>
                     </section>
                 </div>
+        <?php
+        }
+
+        $stmt3 = mysqli_stmt_init($link);
+
+        $query3 = "SELECT COUNT(comentario.texto) FROM comentario WHERE comentario.topico_id_topico = ?";
+
+        if (mysqli_stmt_prepare($stmt3,$query3)){
+                    mysqli_stmt_bind_param($stmt3, 'i' , $id_topico);
+                    mysqli_stmt_execute($stmt3);
+                    mysqli_stmt_bind_result( $stmt3, $total);
+                } else {
+                    echo "ERROR: ". mysqli_error($link);
+                }
+                while (mysqli_stmt_fetch($stmt3)){
+            ?>
 
                 <div class="azul1 borderElement textoClaro titulo py-4 px-3 p-sm-4 p-xl-5">
-                    <h5 class="m-0">Comentários</h5>
+                    <h5 class="m-0">Comentários (<?= $total; } ?>)</h5>
                 </div>
+                <?php
+
+                $stmt1 = mysqli_stmt_init($link);
+
+                $query1 = "SELECT comentario.texto, utilizador.username, comentario.pontuacao, HOUR(TIMEDIFF(NOW(),comentario.data_envio)), MINUTE(TIMEDIFF(NOW(), comentario.data_envio)), utilizador.foto_perfil
+FROM comentario
+INNER JOIN ticket ON ticket.topico_id_topico = comentario.topico_id_topico
+INNER JOIN topico ON topico.id_topico = comentario.topico_id_topico
+INNER JOIN utilizador ON utilizador.id_utilizador = comentario.utilizador_id_utilizador
+WHERE topico.id_topico = ? 
+ORDER BY comentario.data_envio DESC;";
+
+                if (mysqli_stmt_prepare($stmt1,$query1)){
+                    mysqli_stmt_bind_param($stmt1, 'i' , $id_topico);
+                    mysqli_stmt_execute($stmt1);
+                    mysqli_stmt_bind_result( $stmt1, $comment, $pessoa, $comment_score, $comment_hour, $comment_minute, $fotos);
+                } else {
+                    echo "ERROR: ". mysqli_error($link);
+                }
+                while (mysqli_stmt_fetch($stmt1)){
+
+
+                ?>
 
                 <div class="fundoClaro borderElement m-3 py-4 px-3 p-sm-4 p-xl-5">
                     <section class="row">
                         <div class="col-12">
-                            <img class="float-left" src="imgs/icon_avatar.png" height="40px" width="40px">
-                            <p class="float-left font-weight-bold m-0 pt-2 pl-2">João Martins</p>
+                            <img class="float-left" src="imgs/<?= $fotos?>" height="40px" width="40px">
+                            <p class="float-left font-weight-bold m-0 pt-2 pl-2"><?= $pessoa ?></p>
                         </div>
 
                         <div class="col-12 mt-4">
-                            <p>Não percebi nada.</p>
+                            <p><?= $comment ?></p>
 
                             <div class="mt-4 pr-2 mb-0 d-block">
                                 <div class="float-right">
                                     <i class="fas fa-angle-up fa-sm d-block cursor"></i>
+                                    <?= $comment_score ?>
                                     <i class="fas fa-angle-down fa-sm d-block cursor"></i>
                                 </div>
-                                <span class="float-left font-italic small text-secondary pt-2">Postado há 3 minutos</span>
+                                <span class="float-left font-italic small text-secondary pt-2">
+                                <?php
+                                if ($comment_hour == 0){
+                                    echo "Postado há ".$comment_minute." minutos"; }
+                                else if ($comment_hour > 0 && $comment_hour < 24){
+                                    echo "Postado há ".$comment_hour ." horas";}
+                                else if ($comment_hour < 24*7 && $comment_hour > 24){
+                                    $comment_day = intval(($comment_hour / 24));
+                                    echo "Postado há ".$comment_day." dias";
+                                }else{
+                                    $comment_week = intval(($comment_hour / (24*7)));
+                                    echo  "Postado há ". $comment_week." semanas";
+                                }
+                                ?>
+                                </span>
                             </div>
                         </div>
                     </section>
                 </div>
+            <?php } ?>
 
-                <div class="fundoClaro borderElement m-3 py-4 px-3 p-sm-4 p-xl-5">
-                    <section class="row">
-                        <div class="col-12">
-                            <img class="float-left" src="imgs/icon_avatar.png" height="40px" width="40px">
-                            <p class="float-left font-weight-bold m-0 pt-2 pl-2">Mário Guerra</p>
-                        </div>
-
-                        <div class="col-12 mt-4">
-                            <p>Por acaso também quis explorar coisas novas em PHP e mySQL e surgiu-me a mesma preocupação, já que a matéria dos exercícios práticos não se foca tanto nesta vertente.</p>
-
-                            <div class="mt-4 pr-2 mb-0 d-block">
-                                <div class="float-right">
-                                    <i class="fas fa-angle-up fa-sm d-block cursor"></i>
-                                    <i class="fas fa-angle-down fa-sm d-block cursor"></i>
-                                </div>
-                                <span class="float-left font-italic small text-secondary pt-2">Postado há 29 minutos</span>
-                            </div>
-                        </div>
-                    </section>
-                </div>
-
-                <div class="fundoClaro borderElement m-3 py-4 px-3 p-sm-4 p-xl-5">
-                    <section class="row">
-                        <div class="col-12">
-                            <img class="float-left" src="imgs/icon_avatar.png" height="40px" width="40px">
-                            <p class="float-left font-weight-bold m-0 pt-2 pl-2">Joana Silva</p>
-                        </div>
-
-                        <div class="col-12 mt-4">
-                            <p>Posso utilizar Frameworks em vez da abordagem de PDO para prevenir SQL injections? Visto que as Frameworks geralmente são projetadas visando a segurança.</p>
-
-                            <div class="mt-4 pr-2 mb-0 d-block">
-                                <div class="float-right">
-                                    <i class="fas fa-angle-up fa-sm d-block cursor"></i>
-                                    <i class="fas fa-angle-down fa-sm d-block cursor"></i>
-                                </div>
-                                <span class="float-left font-italic small text-secondary pt-2">Postado há 5 horas</span>
-                            </div>
-                        </div>
-                    </section>
-                </div>
-
-                <div class="fundoClaro borderElement m-3 py-4 px-3 p-sm-4 p-xl-5">
-                    <section class="row">
-                        <div class="col-12">
-                            <img class="float-left" src="imgs/icon_avatar.png" height="40px" width="40px">
-                            <p class="float-left font-weight-bold m-0 pt-2 pl-2">Maria Miguel</p>
-                        </div>
-
-                        <div class="col-12 mt-4">
-                            <p>Obrigada pela gravação da reunião. Muito bem explicado!</p>
-
-                            <div class="mt-4 pr-2 mb-0 d-block">
-                                <div class="float-right">
-                                    <i class="fas fa-angle-up fa-sm d-block cursor"></i>
-                                    <i class="fas fa-angle-down fa-sm d-block cursor"></i>
-                                </div>
-                                <span class="float-left font-italic small text-secondary pt-2">Postado há 2 dias</span>
-                            </div>
-                        </div>
-                    </section>
-                </div>
 
                 <a href="#" class="text-secondary small cursor mt-4 ml-3 mb-0">carregar mais comentários...</a>
             </div>
         </article>
     </section>
 
-    <section class="row justify-content-center">
-        <article class="col-12 col-md-11 mt-4 mb-5">
-            <div class="cinzaClaroBg borderElement py-1">
-                <div class="form-group p-3 mt-3">
-                    <div>
-                        <img class="float-left mb-3 ml-2" src="imgs/icon_avatar.png" height="40px" width="40px">
-                        <p class="float-left font-weight-bold m-0 pt-2 pl-2 mb-3">Ricardo Manuel</p>
-                    </div>
-                    <textarea class="form-control borderElement font-italic p-3" id="FormControlTextarea1" rows="2" placeholder="Escreve um comentário..."></textarea>
-
-                    <div class="d-flex justify-content-between small mt-3">
-                        <a href="pedido_tickets.php"><button class="btn borderElement textoClaro comentAnexo titulo ml-1 azul1">Anexar ticket</button></a>
-                        <span><button class="btn rosinhab comentCirculo ml-1" id="enviarTicket"><i class="fas fa-chevron-right fa-lg textoClaro"></i></button></span>
-                    </div>
-                </div>
-            </div>
-        </article>
-    </section>
-</main>
     <?php
 
-}
+    $stmt4 = mysqli_stmt_init($link);
 
-}
-?>
+    $query4 = "SELECT utilizador.foto_perfil, utilizador.username FROM utilizador WHERE utilizador.username = ?";
+
+    if (mysqli_stmt_prepare($stmt4,$query4)){
+        mysqli_stmt_bind_param($stmt4, 's' , $_SESSION['username']);
+        mysqli_stmt_execute($stmt4);
+        mysqli_stmt_bind_result( $stmt4, $lafoto, $lenome);
+    } else {
+        echo "ERROR: ". mysqli_error($link);
+    }
+    while (mysqli_stmt_fetch($stmt4)){
+        echo "<section class='row justify-content-center'>
+        <article class='col-12 col-md-11 mt-4 mb-5'>
+            <div class='cinzaClaroBg borderElement py-1'>
+                <div class='form-group p-3 mt-3'>
+                    <div>
+                        <img class='float-left mb-3 ml-2' src='imgs/$lafoto' height='40px' width='40px'>
+                        <p class='float-left font-weight-bold m-0 pt-2 pl-2 mb-3'> $lenome </p>
+                    </div>
+                    <textarea class='form-control borderElement font-italic p-3' id='FormControlTextarea1' rows='2' placeholder='Escreve um comentário...'></textarea>
+
+                    <div class='d-flex justify-content-between small mt-3'>
+                        <a href='pedido_tickets.php'><button class='btn borderElement textoClaro comentAnexo titulo ml-1 azul1'>Deixar Comentario</button></a>
+                        <span><button class='btn rosinhab comentCirculo ml-1' id='enviarTicket'><i class='fas fa-chevron-right fa-lg textoClaro'></i></button></span>
+                    </div>
+                </div>
+        </article>
+    </section>";
+    }
+    ?>
+
+</main>
 <!-- Footer -->
 <?php include_once "components/cp_footer.php"?>
 
